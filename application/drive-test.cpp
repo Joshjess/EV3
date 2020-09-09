ab14ab1cae59efa0d9d6363ede56c217e2c293b5
alfredez <alfredez008@gmail.com>
2020-09-09 13:45:16 +0200

    new motor fomrula

--- application/drive-test.cpp
+++ application/drive-test.cpp
@@ -123,8 +123,8 @@ void control::turn(int direction)
 
   _state = state_turning;
 
-  _motor_left. set_position_sp( direction).set_speed_sp(100).run_to_rel_pos();
-  _motor_right.set_position_sp( direction).set_speed_sp(100).run_to_rel_pos();
+  _motor_left. set_position_sp( -direction).set_speed_sp(100).run_to_rel_pos();
+  _motor_right.set_position_sp( -direction).set_speed_sp(100).run_to_rel_pos();
 
   while (_motor_left.state().count("running") || _motor_right.state().count("running"))
     this_thread::sleep_for(chrono::milliseconds(10));
