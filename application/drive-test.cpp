9f4c38b1f3950bfa00688722ce74c5984b87af7a
Joshjess <joshuajessy47@gmail.com>
2020-09-16 12:55:50 +0200

    messabe

--- application/drive-test.cpp
+++ application/drive-test.cpp
@@ -267,8 +267,8 @@ void control::remote_loop()
 void control::steer(int motorLeft, int motorRight)
 {
 
-    _motor_left.set_position_sp(0).set_speed_sp(motorLeft).run_to_rel_pos();
-    _motor_right.set_position_sp(0).set_speed_sp(motorRight).run_to_rel_pos();
+    _motor_left.set_position_sp(motorLeft).set_speed_sp(100).run_to_rel_pos();
+    _motor_right.set_position_sp(motorRight).set_speed_sp(100).run_to_rel_pos();
 
     while (_motor_left.state().count("running") || _motor_right.state().count("running"))
         this_thread::sleep_for(chrono::milliseconds(10));
@@ -301,8 +301,8 @@ void control::line_following(){
         int derivative = error - lasterror;
         correction = (kp * error) + (ki * integral) + (kd * derivative);
 
-        motorleftspeed = (motorleftspeed -  correction) / 8 ;
-        motorrightspeed = (motorrightspeed + correction) /8  ;
+        motorleftspeed = (motorleftspeed -  correction)  ;
+        motorrightspeed = (motorrightspeed + correction)   ;
         steer(motorleftspeed, motorrightspeed);
         lasterror = error;
 
